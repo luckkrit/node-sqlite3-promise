e4ad626bb6bd9d764621a47a6a7a6bff6b0ace59
Maxim Sidorov <msidorov@boardmaps.com>
2016-06-29 16:58:49 +0300

    #

--- test.js
+++
@@ -1,6 +0,0 @@
-const Database = require('./index.js');
-
-db = new Database('./db.sqlite');
-
-db.prepare('SELECT * FROM QUESTION LIMIT ?')
-    .then(statement => statement.all(2).then(rows => console.log(rows)))
